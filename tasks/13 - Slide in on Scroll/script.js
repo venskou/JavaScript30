7386170886eb4a613e73a8ac3fef6aa374948905
Uladzislau Vensko <ulad.vensko@gmail.com>
2019-10-04 14:19:56 +0300

    Complete 13 - Slide in on Scroll

---
+++ tasks/13 - Slide in on Scroll/script.js
@@ -0,0 +1,33 @@
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function () {
+    var context = this, args = arguments;
+    var later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderimages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+  sliderimages.forEach(slideImage => {
+    const slideInAt = (window.scrollY + window.innerHeight) - slideImage.height / 2;
+    const imageBottom = slideImage.offsetTop + slideImage.height;
+    const isHalfShown = slideInAt > slideImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      slideImage.classList.add('active');
+    } else {
+      slideImage.classList.remove('active');
+    }
+  })
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
